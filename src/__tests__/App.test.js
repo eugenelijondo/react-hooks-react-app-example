551847354d3029b0e586a4b328d40b7b93b20290
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-02-01 09:22:10 +0300

    creating react app

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -22,16 +22,18 @@ test("should include the <TestComponent />", () => {
   expect(screen.queryByTitle("time video")).toBeInTheDocument();
 });
 
-//   it('should include "Now" in the header instead of a time', () => {
-//     expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
-//     expect(wrapper.find('header').text()).to.include('Now')
-//   });
+// Additional tests you provided
+/*
+test('should include "Now" in the header instead of a time', () => {
+  expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
+  expect(wrapper.find('header').text()).to.include('Now')
+});
 
-//   it('should include the ExampleComponent', () => {
-//     expect(wrapper.text()).to.include('<ExampleComponent />')
-//   });
+test('should include the ExampleComponent', () => {
+  expect(wrapper.text()).to.include('<ExampleComponent />')
+});
 
-//   it('should include the TestComponent', () => {
-//     expect(wrapper.text()).to.include('<TestComponent />')
-//   });
-// });
+test('should include the TestComponent', () => {
+  expect(wrapper.text()).to.include('<TestComponent />')
+});
+*/
